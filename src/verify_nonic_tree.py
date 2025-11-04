1ea18d9c309065a8eb62494d7e324c1be1258dd9
Seewoo Lee <49933279+seewoo5@users.noreply.github.com>
2025-11-04 13:36:47 -0800

    verify_nonic_tree.py (#10)
    * verify_nonic_tree.py
    
    * update comment

---
+++ src/verify_nonic_tree.py
@@ -0,0 +1,34 @@
+"""
+Check that the simple decision tree for nonic fields gives 100% accuracy
+for all nonic Galois extensions in the LMFDB.
+Use `nf_query.sage` to download the data from the LMFDB.
+"""
+import polars as pl
+
+
+def nonic_tree(a1000, a343, a27):
+    """
+    If a_1000 <= 4.5 or a_343 <= 0.5 or a_27 <= 0.5 then return "9T1",
+    else return "9T2".
+    """
+    if a1000 <= 4.5 or a343 <= 0.5 or a27 <= 0.5:
+        return "9T1"
+    else:
+        return "9T2"
+
+
+if __name__ == "__main__":
+    df_nonic = pl.read_csv("data_nf/nf_9_galois.csv")
+    mismatches = []
+    for row in df_nonic.iter_rows(named=True):
+        a1000 = row["a_01000"]
+        a343 = row["a_00343"]
+        a27 = row["a_00027"]
+        galois_label = row["galois_label"]
+        prediction = nonic_tree(a1000, a343, a27)
+        if prediction != galois_label:
+            print("Mismatch found!")
+            label = row["label"]
+            print(f"For Label: {label}, Predicted: {prediction}, Actual: {galois_label}")
+            mismatches.append(label)
+    print(f"Total mismatches: {len(mismatches)}")
